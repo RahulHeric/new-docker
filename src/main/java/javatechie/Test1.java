2eacb30152e080d5fc13c0ef423dbede9dc8648d
rahul.jesuraj <rahul.jesuraj@CHNMCT331389L.mshome.net>
2020-09-01 14:50:16 +0530

    new commit

--- src/main/java/javatechie/Test1.java
+++ src/main/java/javatechie/Test1.java
@@ -3,7 +3,7 @@ package javatechie;
 public class Test1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello from main method");
+		System.out.println("Hello! from main method");
 	}
 
 }
